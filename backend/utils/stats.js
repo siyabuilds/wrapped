54e9f3dde79a2961da68315b2e6f9518df3da8ff
Siyabonga Samson Lukhele <samsonlukhele76@gmail.com>
2025-12-26 18:25:49 +0200

    feat: Add utility data for days of the week

---
+++ backend/utils/stats.js
@@ -0,0 +1,13 @@
+import { config } from "../config";
+
+const DAYS = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+const weekDays = DAYS.slice(1, 6);
+const weekendDays = [DAYS[0], DAYS[6]];
